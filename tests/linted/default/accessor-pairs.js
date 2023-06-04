450d11c1743d22e32deae4285a3637b7b2998175
Stew Eucen <stew.eucen@gmail.com>
2023-06-04 09:40:27 +0900

    Add exempted file for accessor-pairs.js

---
+++ tests/linted/default/accessor-pairs.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const alpha = {
+  sum: 0,
+
+  set sumValue (value) { // ❌ setWithoutGet:true of `accessor-pairs`
+    this.sum += value
+  }
+}
+
+class AccessorPairs {
+  constructor (value) {
+    this.savedValue = value
+  }
+
+  set value (value) { // ❌ setWithoutGet:true & enforceForClassMembers:true of `accessor-pairs`
+    this.savedValue = value
+  }
+}
+
+module.exports = {
+  alpha,
+  AccessorPairs,
+}
