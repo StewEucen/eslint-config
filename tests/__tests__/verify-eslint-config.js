0d3fc1b46ffc7ce254bc7549d62f344d9bce68a3
Stew Eucen <stew.eucen@gmail.com>
2023-06-11 15:21:12 +0900

    Update test to call ESLintInspector

--- tests/__tests__/verify-eslint-config.js
+++ tests/__tests__/verify-eslint-config.js
@@ -6,9 +6,11 @@ const {
 
 describe('verify ESLint config', () => {
   test('should work as expected', async () => {
-    const inspector = await ESLintInspector.createAsyncWithFilePaths([
-      'tests/intents/',
-    ])
+    const inspector = await ESLintInspector.createAsyncWithFilePaths({
+      filePaths: [
+        'tests/intents/',
+      ],
+    })
 
     const unexpectedLog = await inspector.getFormattedLogIfUnexpected()
 
