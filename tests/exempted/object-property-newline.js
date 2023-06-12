ae315cb72d90d12daeda521532031171c63a86ac
Stew Eucen <stew.eucen@gmail.com>
2023-06-12 14:42:21 +0900

    Add exempted file for object-property-newline

---
+++ tests/exempted/object-property-newline.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const alpha = {
+  first: '1st',
+  second: '2nd',
+  third: '3rd',
+}
+
+const beta = { fourth: '4th', fifth: '5th', sixth: '6th' }
+
+const gamma = {
+  seventh: '7th', eighth: '8th', ninth: '9th',
+}
+
+module.exports = {
+  alpha,
+  beta,
+  gamma,
+}
