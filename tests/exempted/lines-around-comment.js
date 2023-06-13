76115cf67fe3353f2efd86b28de8f271181131b6
Stew Eucen <stew.eucen@gmail.com>
2023-06-13 19:56:37 +0900

    Add exempted file for lines-around-comment

---
+++ tests/exempted/lines-around-comment.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const alpha = 'alpha'
+
+/** // ✅ beforeBlockComment:true of `lines-around-comment`
+ * @type {string}
+ */
+const beta = 'beta'
+
+const gammaArray = [
+  /* ✅ allowArrayStart:true of `lines-around-comment` */
+  'first',
+  'second',
+  'third',
+]
+
+function deltaFunc () {
+  /* ✅ allowBlockStart:true of `lines-around-comment` */
+  return 'I am gamma function'
+}
+
+class Epsilon {
+  /* ✅ allowClassStart:true of `lines-around-comment` */
+  constructor () {
+    this.first = 'first'
+  }
+}
+
+const zetaObject = {
+  /* ✅ allowObjectStart:true of `lines-around-comment` */
+  first: 'first',
+}
+
+module.exports = {
+  alpha,
+  beta,
+  gammaArray,
+  deltaFunc,
+  Epsilon,
+  zetaObject,
+}
