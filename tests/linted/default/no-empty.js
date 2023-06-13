25fbc07de95d39397772ee9a73ba8a98d220115d
Stew Eucen <stew.eucen@gmail.com>
2023-06-13 23:15:17 +0900

    Add linted file for no-empty

---
+++ tests/linted/default/no-empty.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const alpha = false
+const beta = 0
+
+if (alpha) {} // ❌ `no-empty`
+
+// eslint-disable-next-line no-unmodified-loop-condition
+while (alpha) {} // ❌ `no-empty`
+
+for (let next = 0; next < beta; next += 1) {} // ❌ `no-empty`
+
+switch (beta) {} // ❌ `no-empty`
+
+try /* ❌ `no-empty` */ {
+} catch (error) /* ❌ `no-empty` */ {
+} finally /* ❌ `no-empty` */ {
+}
+
+module.exports = {
+  alpha,
+  beta,
+}
