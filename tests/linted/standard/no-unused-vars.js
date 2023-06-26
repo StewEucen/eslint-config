f689608250c65a7edce2bb5f755754627bd2f86a
Stew Eucen <stew.eucen@gmail.com>
2023-06-26 14:57:52 +0900

    Add linted file for no-unused-vars

---
+++ tests/linted/standard/no-unused-vars.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const alpha = 1 // ❌ vars:all of `no-unused-vars`
+
+function betaFunc (
+  first, // ✅ args:none of `no-unused-vars`
+  second,
+  third // ✅ args:none of `no-unused-vars`
+) {
+  return second
+}
+
+try {
+  // noop
+} catch (error) { // ✅ caughtErrors:none of `no-unused-vars`
+  // noop
+}
+
+const [
+  gamma,
+  _epsilon, // ✅ destructuredArrayIgnorePattern:^_ of `no-unused-vars`
+  zeta,
+] = [1, 3, 5]
+
+module.exports = {
+  betaFunc,
+  gamma,
+  zeta,
+}
