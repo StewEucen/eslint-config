e4906e23302c6be9f98914658a571361f5118c40
Stew Eucen <stew.eucen@gmail.com>
2023-07-02 00:50:55 +0900

    Add linted file for no-confusing-arrow

---
+++ tests/linted/default/no-confusing-arrow.js
@@ -0,0 +1,67 @@
+'use strict'
+
+// eslint-disable-next-line no-constant-condition
+const alphaFunc = first => 1 // ❌ `no-confusing-arrow`
+  ? first * 2
+  : first - 3
+
+// eslint-disable-next-line no-constant-condition, arrow-parens
+const betaFunc = (first) => 1 // ❌ `no-confusing-arrow`
+  ? first * 2
+  : first - 3
+
+// eslint-disable-next-line no-constant-condition
+const gammaFunc = first => (1 // ✅ allowParens:true of `no-confusing-arrow`
+  ? 2
+  : 3
+)
+
+// eslint-disable-next-line no-constant-condition, arrow-parens
+const deltaFunc = (first) => (1 // ✅ allowParens:true of `no-confusing-arrow`
+  ? 2
+  : 3
+)
+
+// eslint-disable-next-line no-constant-condition
+const epsilonFunc = () => 1 // ❌ onlyOneSimpleParam:false of `no-confusing-arrow`
+  ? 2
+  : 3
+
+// eslint-disable-next-line no-constant-condition
+const zeroFunc = (first, second) => 1 // ❌ onlyOneSimpleParam:false of `no-confusing-arrow`
+  ? 2
+  : 3
+
+const second = 3
+// eslint-disable-next-line no-constant-condition
+const etaFunc = (first = second) => 1 // ❌ onlyOneSimpleParam:false of `no-confusing-arrow`
+  ? 2
+  : 3
+
+// eslint-disable-next-line no-constant-condition
+const thetaFunc = ({ first }) => 1 // ❌ onlyOneSimpleParam:false of `no-confusing-arrow`
+  ? 2
+  : 3
+
+// eslint-disable-next-line no-constant-condition
+const iotaFunc = ([first]) => 1 // ❌ onlyOneSimpleParam:false of `no-confusing-arrow`
+  ? 2
+  : 3
+
+// eslint-disable-next-line no-constant-condition
+const kappaFunc = (...first) => 1 // ❌ onlyOneSimpleParam:false of `no-confusing-arrow`
+  ? 2
+  : 3
+
+module.exports = {
+  alphaFunc,
+  betaFunc,
+  gammaFunc,
+  deltaFunc,
+  epsilonFunc,
+  zeroFunc,
+  etaFunc,
+  thetaFunc,
+  iotaFunc,
+  kappaFunc,
+}
