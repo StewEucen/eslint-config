1cc3d36d0e67c9b54c77001a635c08c13213184b
Stew Eucen <stew.eucen@gmail.com>
2023-07-01 09:12:36 +0900

    Add linted file for spaced-comment

---
+++ tests/linted/standard/spaced-comment.js
@@ -0,0 +1,26 @@
+'use strict'
+
+//First comment. // ❌ `spaced-comment`
+
+/*Second comment. */ // ❌ `spaced-comment`
+/* Third comment.*/ // ❌ block:balanced:true of `spaced-comment`
+
+// ✅ line:exceptions:['/', '-', '+'] `spaced-comment`
+//----
+
+// ✅ line:exceptions:['/', '-', '+'] `spaced-comment`
+//++++
+
+// ✅ line:exceptions:['/', '-', '+'] `spaced-comment`
+//////
+
+// ❌ line:exceptions:['/', '-'] `spaced-comment`
+//****
+
+/******/ // ✅ block:exceptions:['*'] of `spaced-comment`
+/*----*/ // ❌ `spaced-comment`
+/*////*/ // ❌ `spaced-comment`
+
+module.exports = {
+  // none
+}
