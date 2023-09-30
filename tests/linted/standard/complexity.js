bee1170b4d8a74cd3e1b651f1fad0c7cc6eddcf0
Stew Eucen <stew.eucen@gmail.com>
2023-10-01 02:46:02 +0900

    Tidy up linted file for complexity

--- tests/linted/standard/complexity.js
+++ tests/linted/standard/complexity.js
@@ -1,23 +1,25 @@
 'use strict'
 
 /* eslint-disable jsdoc/require-jsdoc */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-restricted-syntax */
 
 function alphaFunc (arg) { // ❌ max:8 of `complexity`
   if (arg === 0) {
     return 'zero' // first path
-  } else if (arg === 1) { // eslint-disable-line no-restricted-syntax
+  } else if (arg === 1) {
     return 'one' // second path
-  } else if (arg === 2) { // eslint-disable-line no-restricted-syntax
+  } else if (arg === 2) {
     return 'two' // third path
-  } else if (arg === 3) { // eslint-disable-line no-restricted-syntax
+  } else if (arg === 3) {
     return 'three' // fourth path
-  } else if (arg === 4) { // eslint-disable-line no-restricted-syntax
+  } else if (arg === 4) {
     return 'four' // fifth path
-  } else if (arg === 5) { // eslint-disable-line no-restricted-syntax
+  } else if (arg === 5) {
     return 'five' // sixth path
-  } else if (arg === 6) { // eslint-disable-line no-restricted-syntax
+  } else if (arg === 6) {
     return 'six' // seventh path
-  } else if (arg === 7) { // eslint-disable-line no-restricted-syntax
+  } else if (arg === 7) {
     return 'seven' // eighth path
   } else { // eslint-disable-line no-else-return
     return 'unknown' // ninth path
@@ -34,14 +36,14 @@ function betaFunc ({ // ❌ max:8 of `complexity`
   seventh,
   eighth,
 }) {
-  first ||= 1 // eslint-disable-line no-param-reassign
-  second &&= 1 // eslint-disable-line no-param-reassign
-  third ??= 1 // eslint-disable-line no-param-reassign
-  fourth ??= 1 // eslint-disable-line no-param-reassign
-  fifth ??= 1 // eslint-disable-line no-param-reassign
-  sixth ??= 1 // eslint-disable-line no-param-reassign
-  seventh ??= 1 // eslint-disable-line no-param-reassign
-  eighth ??= 1 // eslint-disable-line no-param-reassign
+  first ||= 1
+  second &&= 1
+  third ??= 1
+  fourth ??= 1
+  fifth ??= 1
+  sixth ??= 1
+  seventh ??= 1
+  eighth ??= 1
 }
 
 module.exports = {
